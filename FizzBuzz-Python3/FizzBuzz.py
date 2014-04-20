b48a4f55717fb81cbce39ec6493d003af14ab33b
Javier J <javierj@us.es>
2014-04-20 18:42:36 +0200

    FizzBuzz example using Python 3.x and lambdas

---
+++ FizzBuzz-Python3/FizzBuzz.py
@@ -0,0 +1,17 @@
+__author__ = 'Javier'
+
+idontknow = ""
+filters = []
+filters.append(lambda val, prev: "Fizz" if ((val%3) == 0) else prev)
+filters.append(lambda val, prev: str(prev) + "Buzz" if ((val%5) == 0) else prev)
+filters.append(lambda val, prev: val if prev == idontknow else prev)
+
+result =[]
+for i in range(1, 16):
+    value = idontknow
+    for filter in filters:
+        value = filter(i, value)
+    result.append(value)
+
+print(result)
+print ("ok")
---
+++ FizzBuzz-Python3/README.md
@@ -0,0 +1,7 @@
+ Kata FizzBuzz (link) 
+=========================================================
+
+Using Python 3.x (although it should work with Python 2.x).
+
+This is not an "academic" solution for the kata but an example of how resolving it using lamba functions. The idea behind the code is the same than the versions in JavaScript and Java.
+
